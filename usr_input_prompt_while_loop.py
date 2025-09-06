6fb64ebf680bf3060e34ceac57b010feed3eb588
SG <sg@MacBookPro.lan>
2025-09-06 02:31:25 -0500

    created

---
+++ usr_input_prompt_while_loop.py
@@ -0,0 +1,9 @@
+"""
+message = input("Tell me something, and i will repeat :")
+print(message)
+
+name = input("Please enter your name :")
+print(name)
+"""
+age = input("how old are you :")
+print(age)
