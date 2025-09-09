237386178dbfe878d51e12035166f2a3a3f99c4a
SG <sg@MacBookPro.lan>
2025-09-08 23:23:17 -0500

    upated

--- Functions.py
+++ Functions.py
@@ -1,3 +1,4 @@
+"""
 # greet:
 def greet_user():
     "Display a simple greeting."
@@ -23,6 +24,57 @@ fav_book('Alice in WonderLand')
 # Passing Arguments:
 def desc_pet(type, name):
     print(f"\nI have a {type}.")
-    print(f"\nMy {type}'s name is {name.title()}.")
+    print(f"My {type}'s name is {name.title()}.")
 desc_pet('cat', 'kitten')
 # Multiple fuction calls:
+desc_pet('dog', 'ginger')
+# Default value:
+def desc_vehicle(name, type = 'car'):
+    print(f"\nI have a {type}.")
+    print(f"My {type}'s name is {name.title()}.")
+desc_vehicle('acura','suv')
+desc_vehicle('subaru')
+desc_vehicle('nissan','')
+"""
+print("******* Exercise 8.3 *******")
+def make_shirt(size, desc):
+    print(f"\nSize of the Shirt is : {size}")
+    print(f"Its theme is : {desc.upper()}")
+make_shirt('Medium', 'Just Do It')
+make_shirt('Small', 'for successful living')
+
+print("******* Exercise 8.4 *******")
+def make_shirt(size, desc='I love Python'):
+    print(f"\nSize of the Shirt is : {size}")
+    print(f"Its theme is : {desc.upper()}")
+make_shirt('Medium')
+make_shirt('Large')
+make_shirt('Small', 'for successful living')
+
+print("******* Exercise 8.5 *******")
+def city_desc(city, country = 'India'):
+    print(f"\n{city} is in {country.title()}")
+
+city_desc('ongole')
+city_desc('amaravathi')
+city_desc('dallas', 'usa')
+
+# Return Values:
+def formatted_name(fname, lname):
+    fullname = f"{fname} {lname}"
+    return fullname.title()
+
+artist = formatted_name('tarak', 'nandamuri')
+print(artist)
+
+# optional argument:
+def name(fname, lname, mname = ''):
+    if mname:
+        fullname = f"{fname} {mname} {lname}"
+    else:
+        fullname = f"{fname} {lname}"
+    return fullname.title()
+artist = name('siva', 'kumar','gurram')
+print(artist)
+artist = name('vikranth','gurram')
+print(artist)
