2ef6d5d9237c8d0cb0594202e6c70402861db6b0
SG <sg@MacBookPro.lan>
2025-09-17 23:37:26 -0500

    del

--- Modules/pizza.py
+++
@@ -1,6 +0,0 @@
-
-def make_pizza(size, *toppings):
-    "Summarize the pizza we are about to make."
-    print(f"\nMaking a {size}-inch pizza with following toppings:")
-    for all_toppings in toppings:
-        print(f"- {all_toppings}")
